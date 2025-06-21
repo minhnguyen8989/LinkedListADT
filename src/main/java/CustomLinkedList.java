9bd57eb8fb6be47ce7b5d11b36851bf0329a5f21
Minh Nguyen <122935530+minhnguyen8989@users.noreply.github.com>
2025-06-21 17:47:58 -0500

    completed

---
+++ src/main/java/CustomLinkedList.java
@@ -0,0 +1,70 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomLinkedList {
+    private Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void delete(int data) {
+        if (head == null) return;
+
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.data != data) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+
+    public Iterator<Integer> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private class LinkedListIterator implements Iterator<Integer> {
+        private Node current = head;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            int data = current.data;
+            current = current.next;
+            return data;
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomLinkedList linkedList = new CustomLinkedList();
+
+        linkedList.insert(1);
+        linkedList.insert(2);
+        linkedList.insert(3);
+        linkedList.insert(2);
+        linkedList.insert(4);
+        linkedList.insert(5);
+        linkedList.insert(1);
+
+        try (Scanner scanner = new Scanner(new File("input.txt"))) {
+            while (scanner.hasNextInt()) {
+                linkedList.insert(scanner.nextInt());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            return;
+        }
+
+        System.out.print("List contents: ");
+        Iterator<Integer> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+
+        linkedList.delete(1);
+
+        System.out.print("After deletion: ");
+        iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+}
